e607161f768a1db1ca7ea946335edd54996e2d8d
francisco-1998 <francmarinc@gmail.com>
2021-03-03 09:52:47 -0500

    Validando el form

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,17 +1,37 @@
 import React, { Fragment } from 'react';
+import Error from './Error'
 
 const Pregunta = () => {
+
+    const [presupuesto, setPresupuesto] = React.useState(0);
+    const [error, setError] = React.useState(false);
+
+    const sendPresupuesto = (e) => {
+        e.preventDefault();
+        if (presupuesto < 1 || isNaN(presupuesto)) {
+            setError(true);
+            return;
+        }
+        setError(false);
+        console.log(presupuesto);
+    }
+
     return (
         <Fragment>
             <h2>Ingrese el presupuesto de esta semana</h2>
-            <form>
+            {
+                error ? <Error message='El presupuesto es incorrecto' />
+                      : null
+            }
+            <form onSubmit={sendPresupuesto}>
                 <input type="number"
-                       name="presupuesto"
-                       className="u-full-width" 
-                       placeholder="Escriba su presupuesto"/>
-                       
-                <button type="button" 
-                        className="button-primary u-full-width">
+                    name="presupuesto"
+                    className="u-full-width"
+                    placeholder="Escriba su presupuesto"
+                    onChange={e => setPresupuesto(parseInt(e.target.value))} />
+
+                <button type="submit"
+                    className="button-primary u-full-width">
                     Ingresar presupuesto
                 </button>
             </form>
